72558c22fdf35973368975d10d0069dcd15cee22
Jonai <jonaybolanosmolina@gmail.com>
2024-10-29 16:47:45 +0000

    arreglado error de añadir imagen y bloqueo en la url

--- frontend/my-react-app/src/views/addProduct.jsx
+++ frontend/my-react-app/src/views/addProduct.jsx
@@ -15,6 +15,20 @@ function AddProduct() {
     // Manejador de archivo de imagen
     const handleImageFileChange = (e) => {
         setImageFile(e.target.files[0]);
+        setImageUrl(''); // Limpia el campo de URL si se selecciona un archivo
+    };
+
+    // Manejador de cambio en el campo de URL
+    const handleImageUrlChange = (e) => {
+        setImageUrl(e.target.value);
+        setImageFile(null); // Limpia el archivo si se introduce una URL
+    };
+
+    // Función para limpiar el campo de imagen y habilitar el campo de URL
+    const resetImageFile = () => {
+        setImageFile(null); // Limpia el archivo
+        // Limpia también el input de archivo, si es necesario
+        document.getElementById('imageFileInput').value = ''; // Limpia el input del archivo
     };
 
     const handleSubmit = async (e) => {
@@ -79,7 +93,7 @@ function AddProduct() {
                         className="form-control mb-3" 
                         placeholder="URL de la imagen" 
                         value={imageUrl} 
-                        onChange={(e) => setImageUrl(e.target.value)} 
+                        onChange={handleImageUrlChange} // Usa el nuevo manejador para el campo URL
                         disabled={imageFile !== null} // Desactiva si hay un archivo seleccionado
                     />
                     <input 
@@ -87,12 +101,20 @@ function AddProduct() {
                         className="form-control mb-3" 
                         onChange={handleImageFileChange} 
                         accept="image/*"
+                        id="imageFileInput" // Asigna un ID para poder limpiar el input
                     />
+                    {/* Botón para resetear el campo de imagen */}
+                    {imageFile && (
+                        <button type="button" className="btn btn-danger my-2" onClick={resetImageFile}>
+                            Eliminar Imagen
+                        </button>
+                    )}
                 </div>
-                <button type="submit" className="btn btn-primary">Añadir Producto</button>
+                <button type="submit" className="btn btn-primary w-100">Añadir Producto</button>
             </form>
         </div>
     );
 }
 
 export default AddProduct;
+
